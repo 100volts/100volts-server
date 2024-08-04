7172675af82d624fc9c99a8b22cf821258754227
IvoRum <ivailo.rumenov@outlook.com>
2024-08-04 23:13:35 +0300

    Updated SecurityConfig

--- volts-server/src/main/java/org/lci/volts/server/controller/ElMeterController.java
+++ volts-server/src/main/java/org/lci/volts/server/controller/ElMeterController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class ElMeterController {
     private final ElMerterService elMerterService;
 
-    @PostMapping
+    @PostMapping("/data")
     public ResponseEntity<ElMeterReadResponse> insertData(
             @RequestBody ElMeterReadRequest request
     ) {
---
+++ volts-server/src/test/java/org/lci/volts/server/controller/ElMeterControllerTest.java
@@ -0,0 +1,7 @@
+package org.lci.volts.server.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ElMeterControllerTest {
+
+}
