6054cd187432b53fc724f9b03b4c4322123464c3
irumenov <ivaylo.rumenov2@dxc.com>
2024-08-11 18:12:51 +0300

    Added json properies to user responce

--- volts-server/src/main/java/org/lci/volts/server/model/responce/UserDataResponse.java
+++ volts-server/src/main/java/org/lci/volts/server/model/responce/UserDataResponse.java
@@ -1,5 +1,7 @@
 package org.lci.volts.server.model.responce;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +9,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonRootName("company_user_data")
 public class UserDataResponse {
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
+    @JsonProperty("email")
     private String email;
 }
