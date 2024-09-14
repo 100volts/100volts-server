c2ef5882e6f2f36be6f991b97178bda34efb6478
IvoRum <ivailo.rumenov@outlook.com>
2024-09-14 09:41:18 +0300

    Removed unused imports

--- volts-server/src/main/java/org/lci/volts/server/repository/WaterDataRepository.java
+++ volts-server/src/main/java/org/lci/volts/server/repository/WaterDataRepository.java
@@ -4,7 +4,6 @@ import org.lci.volts.server.persistence.water.WaterData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
