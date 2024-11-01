e12a52db6b8263a37d5bc00543e75b9291ddb320
irumenov <ivaylo.rumenov2@dxc.com>
2024-11-01 13:18:55 +0200

    Added h2 to dependencies and configurations for the test

--- volts-server/src/test/java/org/lci/volts/server/VoltsServerApplicationTests.java
+++ volts-server/src/test/java/org/lci/volts/server/VoltsServerApplicationTests.java
@@ -1,11 +1,13 @@
 package org.lci.volts.server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VoltsServerApplicationTests {
-
     @Test
     void contextLoads() {
     }
